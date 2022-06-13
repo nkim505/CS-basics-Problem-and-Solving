c42b7534dadfe55feda9729a4b31e68272264618
Nari Kim <59485802+nkim505@users.noreply.github.com>
2022-06-13 21:22:50 +0900

    Update 상수.py

--- Baekjoon/상수.py
+++ Baekjoon/상수.py
@@ -1,3 +1,4 @@
+# 내 코드 
 a, b = input().split()
 
 a_rev = a[::-1]
@@ -5,3 +6,16 @@ b_rev = b[::-1]
 
 result = max(a_rev, b_rev)
 print(result)
+
+
+
+# 시현님 코드
+a, b = input().split()
+
+new_a = a[::-1]
+new_b = b[::-1]
+
+if int(new_a) < int(new_b):
+    print(int(new_b))
+else:
+    print(int(new_a))
