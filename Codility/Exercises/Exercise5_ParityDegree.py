b6b0540d289f501b3663336a12ee7de3fdcadf3b
Nari Kim <59485802+nkim505@users.noreply.github.com>
2022-06-18 13:38:08 +0900

    Update Exercise5_ParityDegree.py

--- Codility/Exercises/Exercise5_ParityDegree.py
+++ Codility/Exercises/Exercise5_ParityDegree.py
@@ -44,4 +44,29 @@ def solution(N):
     return result
 
 # score 100%
+# 하지만 Integer Decimal Occurrence 라는 말이 뜬다. 이게 뭐지?
+# "If you’re using this test for recruitment we recommend swapping it with: IntegerDecimalOccurrence."
+# 정수로만 해줘봄
+
+
+#### 3번째 시도
+def solution(N):
+    # write your code in Python 3.6
+    
+    result = 0
+    
+    # 홀수인 경우
+    if N % 2 != 0:
+        result = 0
+    
+    # 소인수분해하기
+    else:
+        count = 0
+        while N % 2 == 0:
+            N = int(N / 2)
+            count += 1
+        
+        result = count
+    
+    return result
   
