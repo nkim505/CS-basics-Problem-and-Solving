29cc33680bfb621f90e318973adae51b1d21c605
Nari Kim <59485802+nkim505@users.noreply.github.com>
2022-06-18 13:28:57 +0900

    Create Exercise5_ParityDegree.py

---
+++ Codility/Exercises/Exercise5_ParityDegree.py
@@ -0,0 +1,26 @@
+def solution(N):
+    # write your code in Python 3.6
+    
+    result = 0
+    
+    # 홀수인 경우
+    if N % 2 != 0:
+        result = -1
+    
+    # 소인수분해하기
+    else:
+        count = 0
+        while N % 2 == 0:
+            N = N / 2
+            count += 1
+        
+        result = count
+    
+    return result
+  # 결과 correctness: 50%
+  # -1이 아니고 0이 나와야하나 봄(홀수일 때)
+  
+  
+  
+  ## 다시 시도
+  
