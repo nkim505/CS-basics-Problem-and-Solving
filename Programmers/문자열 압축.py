5e937f666df3f46699ca01c66790505e5b719616
Nari Kim <59485802+nkim505@users.noreply.github.com>
2022-06-23 20:55:08 +0900

    Create 문자열 압축.py

---
+++ Programmers/문자열 압축.py
@@ -0,0 +1,21 @@
+# 실행시간 초과
+def solution(s):
+    answer = len(s)
+    max_num = len(s)//2 
+    x = 2 # 세는 갯수
+    cnt = 1
+    lines = ""
+    while x <= max_num:
+        for i in range(0, len(s), x):
+            if s[i:i+x] == s[i+x:i+x+x]:
+                cnt += 1
+            else:
+                if cnt == 1:
+                    lines = s[i:i+x]
+                elif cnt > 1:
+                    lines += str(cnt) + s[i:i+x]
+                    cnt = 1 # 다시 cnt 초기화
+    new_answer = len(lines)             
+    answer = min(answer, new_answer)
+    x += 1 # x+1개씩 슬라이싱 하는 단계로 넘어감
+    return answer
