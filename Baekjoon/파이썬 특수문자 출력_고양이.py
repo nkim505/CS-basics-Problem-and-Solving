6210cbc564ab492d5d53d5bc6c9d0c30cb9abd23
Nari Kim <59485802+nkim505@users.noreply.github.com>
2022-06-29 18:16:44 +0900

    Create 파이썬 특수문자 출력_고양이.py

---
+++ Baekjoon/파이썬 특수문자 출력_고양이.py
@@ -0,0 +1,9 @@
+# 파이썬 특수문자 출력
+# https://velog.io/@semi_kimm/Python-%ED%8A%B9%EC%88%98%EB%AC%B8%EC%9E%90-%EC%B6%9C%EB%A0%A5
+
+print("\    /\\")
+print(" )  ( \')")
+print("(  /  )")
+print(" \\(__)|")
+
+
