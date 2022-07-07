0ad29581a56aca71fd5efb19331dcbc1cabee21b
Nari Kim <59485802+nkim505@users.noreply.github.com>
2022-07-07 17:41:15 +0900

    edit code

--- 이것이코딩테스트다/Part3/구현_치킨배달.py
+++ 이것이코딩테스트다/Part3/구현_치킨배달.py
@@ -1,8 +1,17 @@
 from numpy import append
 
 def calculate_dist(loc1, loc2):
-    result = abs(loc1[1] - loc2[1]) + abs(loc1[2] - loc2[2])
+    result = abs(loc1[0] - loc2[0]) + abs(loc1[1] - loc2[1])
     return result
+
+def make_houselist(graph, n):
+    house_list = []
+    for i in range(n):
+        for j in range(n):
+            if graph[i][j] == 1:
+                house_list.append((i,j))
+    return house_list
+
 
 N, M = map(int, input("input numbers: ").split())
 
@@ -12,16 +21,17 @@ for _ in range(N):
     tmp = list(map(int, input().split()))
     graph.append(tmp)
 
-
-
-
-
+house_list = make_houselist(graph, N)
 all_dist = []
-dist = []
 
+for house in house_list:
+    dist = []
+    for i in range(N):
+        for j in range(N):
+            if graph[i][j] == 2: #해당 위치가 치킨집이면 치킨거리 계산
+                tmp = calculate_dist(house,(i,j))
+                dist.append(tmp)
 
-
-
-
+    all_dist.append(min(dist))
 
 print(sum(all_dist))
