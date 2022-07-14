0bda23d2b33d2978f76df451e910f35501566eca
Nari Kim <59485802+nkim505@users.noreply.github.com>
2022-07-14 22:50:30 +0900

    Update 치킨배달.py

--- Baekjoon/치킨배달.py
+++ Baekjoon/치킨배달.py
@@ -1,3 +1,43 @@
+from itertools import combinations
+
+n, m = map(int,input().split())
+array = []
+for _ in range(n):
+    array.append(list(map(int,input().split())))
+
+# 치킨집의 위치
+chicken_loc = []
+for i in range(n):
+    for j in range(n):
+        if array[i][j] == 2:
+            chicken_loc.append((i,j))
+
+# 집들의 위치
+house_loc = []
+for i in range(n):
+    for j in range(n):
+        if array[i][j] == 1:
+            house_loc.append((i,j))
+
+chicken_comb = list(combinations(chicken_loc,m))
+answer_list = []
+dist_list = []
+distance_list = []
+
+for co in chicken_comb: # 치킨집 좌표의 조합중에서 최단거리 찾기
+    for i,j in house_loc:
+        for x,y in co:  # 집별로 치킨거리 구하기
+            dist_list.append(abs(i-x) + abs(j-y))
+        distance_list.append(min(dist_list))
+        dist_list = []
+    answer_list.append(sum(distance_list))
+    distance_list = []
+
+print(min(answer_list))
+
+
+'''
+# 잘못 푼 코드
 def calculate_dist(loc1, loc2):
     result = abs(loc1[0] - loc2[0]) + abs(loc1[1] - loc2[1])
     return result
@@ -34,13 +74,4 @@ for chicken in chicken_list:
 
 print(all_dist)
 
-'''
-all_dist = sorted(all_dist, key = lambda x : x[0])
-
-all_dist = all_dist[0:M]
-
-
-# to be continue..
-
-print(all_dist)
 '''
